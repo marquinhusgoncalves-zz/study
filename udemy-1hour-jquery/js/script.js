e0c16c3924927e3f5c7d96776cd6e7aa70a1408b
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2017-09-21 20:59:51 -0300

    Finish Section 3

--- udemy-1hour-jquery/js/script.js
+++ udemy-1hour-jquery/js/script.js
@@ -1,12 +1,33 @@
 (function() {
   'use strict';
 
-  var tagElementh1 = $('h1');
-  var tagElementh2 = $('h2');
-  var tagElementBtn = $('button');
+  // var tagElementh1 = $('h1');
+  // var tagElementh2 = $('h2');
+  // var tagElementBtn1 = $('#button1');
+  // var tagElementBtn2 = $('#button2');
 
-  tagElementBtn.click(function () {
-    tagElementh1.fadeOut();
+  // tagElementBtn1.click(function () {
+  //   tagElementh1.fadeOut();
+  // });
+
+  // tagElementBtn2.click(function () {
+  //   tagElementh1.fadeIn();
+  // });
+
+  var tagElementBtn1 = $('#button1');
+  var tagElementBtn2 = $('#button2');
+  var tagElementBtn3 = $('#button3');
+
+  tagElementBtn1.click(function () {
+    $('.tag-elements').fadeOut(2000);
+  });
+
+  tagElementBtn2.click(function () {
+    $('.tag-elements').fadeIn(3000);
+  });
+
+  tagElementBtn3.click(function () {
+    $('.tag-elements').toggle(1000);
   });
 
 })();

0d6ff97e3a11772b45e31952c643a446de6845c2
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2017-09-25 20:10:29 -0300

    Finish course

--- udemy-1hour-jquery/js/script.js
+++ udemy-1hour-jquery/js/script.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-
+  // How to use jQuery
   // var tagElementh1 = $('h1');
   // var tagElementh2 = $('h2');
   // var tagElementBtn1 = $('#button1');
@@ -14,20 +14,46 @@
   //   tagElementh1.fadeIn();
   // });
 
+  // How to use jQuery and your methods
+  // var tagElementBtn1 = $('#button1');
+  // var tagElementBtn2 = $('#button2');
+  // var tagElementBtn3 = $('#button3');
+
+  // tagElementBtn1.click(function () {
+  //   $('.tag-elements').fadeOut(2000);
+  // });
+
+  // tagElementBtn2.click(function () {
+  //   $('.tag-elements').fadeIn(3000);
+  // });
+
+  // tagElementBtn3.click(function () {
+  //   $('.tag-elements').toggle(1000);
+  // });
+
+  //How to use animation
   var tagElementBtn1 = $('#button1');
   var tagElementBtn2 = $('#button2');
   var tagElementBtn3 = $('#button3');
+  var tagElementBtn4 = $('#button4');
 
   tagElementBtn1.click(function () {
-    $('.tag-elements').fadeOut(2000);
+    $('.tag-elements').animate({ opacity: .5 });
   });
 
   tagElementBtn2.click(function () {
-    $('.tag-elements').fadeIn(3000);
+    $('.square').animate({ left: '100px' }, 'slow');
   });
 
   tagElementBtn3.click(function () {
-    $('.tag-elements').toggle(1000);
+    $('.square').animate({ left: '0' }, 'fast')
+  });
+
+  tagElementBtn4.click(function () {
+    $('.square').animate({
+      height: '100px',
+      width: '100px'
+    }, 'slow');
   });
 
 })();
